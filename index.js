de67bd866bf221d08d803be04359fcdebf0ff665
vignesh.jc <vickyjaya007@gmail.com>
2021-09-01 23:56:04 +0530

    Cookie setting

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ var app=express();
 var logger=require('morgan');
 var bodyParser=require('body-parser');
 var cookieParse=require('cookie-parser');
+var session = require('express-session');
 
 app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.get('/',function(req,res,next){
-    console.log('***Page loaded***');
-    res.send("***Page Loaded***");
+    console.log(req.headers.cookie);
+    if(req.headers.cookie==null){
+        res.send("Not authenticated");  
+    }
+    next();
+});
+app.get('/',function(req,res){
+    res.send("Logged in");
+});
+app.get('/login',function(req,res){
+    console.log(req.headers.cookie);
+    if(req.headers.cookie!==null)
+    res.cookie('name', 'express');
+    res.redirect("/");
+});
+app.get('/logout',function(req,res){
+    console.log(req.headers.cookie);
+    res.clearCookie('name');
+    res.redirect('/');
 });
 
 module.exports=app;
